075d036806dece3e28bf05dd04c0110a5a000943
Rajneesh Kumar <192125aryaraj@gmail.com>
2023-12-26 21:58:07 +0530

    logout button added

--- app/src/main/java/com/example/journal/JournalListActivity.java
+++ app/src/main/java/com/example/journal/JournalListActivity.java
@@ -1,14 +1,60 @@
 package com.example.journal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.journal.databinding.ActivityJournalListBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class JournalListActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private StorageReference storageReference;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    ActivityJournalListBinding journalBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_journal_list);
+        journalBinding = ActivityJournalListBinding.inflate(getLayoutInflater());
+        setContentView(journalBinding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_add){
+            // Adding Journal
+            if(user!=null && firebaseAuth != null){
+                startActivity(new Intent(JournalListActivity.this,PostJournalActivity.class));
+            }
+
+        } else if (id == R.id.action_signout) {
+            // Signout User
+            if(user!=null && firebaseAuth!=null){
+                startActivity(new Intent(JournalListActivity.this,MainActivity.class));
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/journal/LoginActivity.java
+++ app/src/main/java/com/example/journal/LoginActivity.java
@@ -1,18 +1,44 @@
 package com.example.journal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.journal.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Collection;
+
+import model.Journal;
+import util.JournalApi;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding loginBinding;
+    
+    // Firebase Instance Variable
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(loginBinding.getRoot());
 
+        firebaseAuth = FirebaseAuth.getInstance();
         loginBinding.createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,7 +58,48 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                startActivity(new Intent());
                 // Move to Journal Page
+                loginEmailPasswordUser(loginBinding.email.getText().toString().trim(),
+                        loginBinding.password.getText().toString().trim());
             }
         });
+    }
+
+    private void loginEmailPasswordUser(String email, String password) {
+        loginBinding.loginProgress.setVisibility(View.VISIBLE);
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            firebaseAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            assert user != null;
+                            String currentUserId  = user.getUid();
+                            collectionReference.whereEqualTo("userId",currentUserId)
+                                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                                            loginBinding.loginProgress.setVisibility(View.INVISIBLE);
+                                            assert value != null;
+                                            if(error != null){}
+                                            if(!value.isEmpty()){
+                                                for(QueryDocumentSnapshot snapshot:value){
+                                                    JournalApi journalApi = JournalApi.getInstance();
+                                                    journalApi.setUsername(snapshot.getString("username"));
+                                                    journalApi.setUserId(snapshot.getString("userId"));
+                                                    // Go to List Activity
+                                                    startActivity(new Intent(LoginActivity.this,PostJournalActivity.class));
+                                                }
+                                            }
+                                        }
+                                    });
+                        }
+                    }).addOnFailureListener(e -> {
+                        loginBinding.loginProgress.setVisibility(View.INVISIBLE);
+                        Toast.makeText(this, "Failed to Login\nTry Again!", Toast.LENGTH_SHORT).show();
+                    });
+        }else{
+            loginBinding.loginProgress.setVisibility(View.INVISIBLE);
+            Toast.makeText(LoginActivity.this,"Please enter email and address",Toast.LENGTH_SHORT).show();
+        }
     }
 }
